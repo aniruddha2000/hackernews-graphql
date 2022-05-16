ae328c6f1c8b488ae13a36da919f2ec319c6bb11
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-05-16 21:13:56 +0530

    Update resolver
    Signed-off-by: Aniruddha Basak <codewithaniruddha@gmail.com>

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -1,8 +1,5 @@
 package graph
 
-// This file will be automatically regenerated based on the schema, any resolver implementations
-// will be copied through when generating and any unknown code will be moved to the end.
-
 import (
 	"context"
 	"errors"
@@ -11,57 +8,106 @@ import (
 
 	"github.com/aniruddha2000/hackernews/graph/generated"
 	"github.com/aniruddha2000/hackernews/graph/model"
+	"github.com/aniruddha2000/hackernews/internal/auth"
 	"github.com/aniruddha2000/hackernews/internal/links"
+	"github.com/aniruddha2000/hackernews/internal/users"
+	"github.com/aniruddha2000/hackernews/pkg/jwt"
 )
 
 func (r *mutationResolver) CreateLink(ctx context.Context, input model.NewLink) (*model.Link, error) {
-	newLink := links.Links{
+	user := auth.ForContext(ctx)
+	if user == nil {
+		return &model.Link{}, fmt.Errorf("access denied")
+	}
+
+	newLink := links.Link{
 		Title:   input.Title,
 		Address: input.Address,
+		User:    user,
 	}
 	linkID := newLink.Save()
 
+	graphqlUser := &model.User{
+		ID:   user.ID,
+		Name: user.Username,
+	}
 	return &model.Link{
 		ID:      strconv.FormatInt(linkID, 10),
 		Title:   newLink.Title,
 		Address: newLink.Address,
+		User:    graphqlUser,
 	}, nil
 }
 
 func (r *mutationResolver) CreateUser(ctx context.Context, input model.NewUser) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	var user = users.User{
+		Username: input.Username,
+		Password: input.Password,
+	}
+	err := user.Create()
+	if err != nil {
+		return "", err
+	}
+	token, err := jwt.GenerateToken(user.Username)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 func (r *mutationResolver) UpdateLink(ctx context.Context, id string, input model.NewLink) (*model.Link, error) {
-	link := links.Links{
-		Title:   input.Title,
-		Address: input.Address,
-	}
-	rowsAffected := link.Update(id)
-	if rowsAffected == 0 {
-		return nil, errors.New("zero rows affected")
-	}
-	return &model.Link{
-		ID:      id,
-		Title:   link.Title,
-		Address: link.Address,
-	}, nil
+	// link := links.Link{
+	// 	Title:   input.Title,
+	// 	Address: input.Address,
+	// }
+	// rowsAffected := link.Update(id)
+	// if rowsAffected == 0 {
+	// 	return nil, errors.New("zero rows affected")
+	// }
+	// return &model.Link{
+	// 	ID:      id,
+	// 	Title:   link.Title,
+	// 	Address: link.Address,
+	// }, nil
+	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *mutationResolver) DeleteLink(ctx context.Context, id string) (string, error) {
-	rowsAffected := links.Delete(id)
-	if rowsAffected == 0 {
-		return "", errors.New("zero rows affected")
-	}
-	return fmt.Sprintf("%v rows affected", rowsAffected), nil
+	// rowsAffected := links.Delete(id)
+	// if rowsAffected == 0 {
+	// 	return "", errors.New("zero rows affected")
+	// }
+	// return fmt.Sprintf("%v rows affected", rowsAffected), nil
+	panic(fmt.Errorf("not implemented"))
 }
 
 func (r *mutationResolver) Login(ctx context.Context, input model.Login) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	var user = users.User{
+		Username: input.Username,
+		Password: input.Password,
+	}
+
+	correct := user.Authenticate()
+	if !correct {
+		return "", errors.New("wrong username or password")
+	}
+	token, err := jwt.GenerateToken(user.Username)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 func (r *mutationResolver) RefreshToken(ctx context.Context, input model.RefreshTokenInput) (string, error) {
-	panic(fmt.Errorf("not implemented"))
+	username, err := jwt.ParseToken(input.Token)
+	if err != nil {
+		return "", fmt.Errorf("access denied")
+	}
+	token, err := jwt.GenerateToken(username)
+	if err != nil {
+		return "", err
+	}
+	return token, nil
 }
 
 func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
@@ -75,12 +121,13 @@ func (r *queryResolver) Links(ctx context.Context) ([]*model.Link, error) {
 }
 
 func (r *queryResolver) Link(ctx context.Context, id string) (*model.Link, error) {
-	link := links.Get(id)
-	return &model.Link{
-		ID:      link.ID,
-		Title:   link.Title,
-		Address: link.Address,
-	}, nil
+	// link := links.Get(id)
+	// return &model.Link{
+	// 	ID:      link.ID,
+	// 	Title:   link.Title,
+	// 	Address: link.Address,
+	// }, nil
+	panic(fmt.Errorf("not implemented"))
 }
 
 // Mutation returns generated.MutationResolver implementation.
